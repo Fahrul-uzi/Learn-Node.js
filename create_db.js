55bb4316da04df34e7ca4bbfaa2a9f0b2e451ec7
PickHD <44204116+PickHD@users.noreply.github.com>
2019-01-28 16:33:52 +0700

    Create create_db.js

---
+++ create_db.js
@@ -0,0 +1,21 @@
+//importing and Connecting to mysql
+var mysql=require('mysql');
+//i use xampp
+var db=mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"",
+	database:""
+});
+
+
+//create your database
+db.connect(function (err) {
+	if (err) throw err;
+  //sql function
+	let sql = "CREATE DATABASE your_database_name";
+	db.query(sql,function(err,result){
+		if (err) throw err;
+		console.log("your text err here");
+	});
+});
